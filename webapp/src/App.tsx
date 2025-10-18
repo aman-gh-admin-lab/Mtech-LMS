6a23a5f2ecc664209886a4e70effbae1debc1b59
Amanulla Mohammad <mdaman1326@gmail.com>
2025-10-18 06:45:48 -0700

    Adding authentication dev

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,12 +4,23 @@ import { LocationGenerics, routes } from "./routes";
 import MainLayout from "./layouts/MainLayout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"; // Add these lines
 
 const location = new ReactLocation<LocationGenerics>();
 
 function App() {
   return (
     <>
+      {/* Clerk Auth Header */}
+      <header style={{ display: "flex", justifyContent: "flex-end", padding: "1rem" }}>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+
       <Router location={location} routes={routes}>
         <MainLayout>
           <Outlet />
