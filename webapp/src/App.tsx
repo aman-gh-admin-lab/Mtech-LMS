2225a5a9f28d3de28d11ca50c9a19efd1a4a7f31
Amanulla Mohammad <mdaman1326@gmail.com>
2025-10-18 21:25:22 -0700

    Adding authentication change redirect

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,31 +4,34 @@ import { LocationGenerics, routes } from "./routes";
 import MainLayout from "./layouts/MainLayout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
-import { useUser, SignInButton, UserButton } from "@clerk/clerk-react"; // Import useUser
+import { useUser, RedirectToSignIn, UserButton } from "@clerk/clerk-react"; // Note: add UserButton import
 
 const location = new ReactLocation<LocationGenerics>();
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded) {
+    // Optionally show a loading indicator here
+    return null;
+  }
+
+  if (!isSignedIn) {
+    return <RedirectToSignIn />;
+  }
 
   return (
     <>
-      {/* Auth header */}
+      {/* Auth Header */}
       <header style={{ display: "flex", justifyContent: "flex-end", padding: "1rem" }}>
-        {!isSignedIn ? (
-          <SignInButton />
-        ) : (
-          <UserButton />
-        )}
+        <UserButton />
       </header>
-
-      {/* Your existing router and layout */}
+      {/* Main routing and layout */}
       <Router location={location} routes={routes}>
         <MainLayout>
           <Outlet />
         </MainLayout>
       </Router>
-
       <ToastContainer position="top-center" />
     </>
   );
