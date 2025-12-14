a5b9c1bc31682101d63b2271e4e784ae92bc9096
Amanulla Mohammad <mdaman1326@gmail.com>
2025-12-14 06:54:36 -0800

    Updated changes again

--- webapp/src/components/Banner.tsx
+++ webapp/src/components/Banner.tsx
@@ -5,7 +5,7 @@ function Banner() {
     >
       <div className="text-center">
         <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
-          Welcome To My Mtech Program!
+          Welcome To LMS Project!
         </h1>
       </div>
     </div>
